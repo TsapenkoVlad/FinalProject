aa66192580340255a9e38f81757fa65d491e343a
TsapenkoVlad <vlad.czapenko5@gmail.com>
2023-07-19 14:36:02 +0300

    finalLocalStorage

---
+++ tickets/tickets.js
@@ -0,0 +1,20 @@
+let cityTicket = localStorage.getItem('city');
+let city2 = document.querySelector('#city');
+city2.innerHTML = cityTicket;
+
+let timeTicket = localStorage.getItem('time');
+let time2 = document.querySelector('#time');
+time2.innerHTML = timeTicket;
+
+let dateTicket = localStorage.getItem('date');
+let date2 = document.querySelector('#date');
+date2.innerHTML = dateTicket; 
+
+let seatTicket = localStorage.getItem('seat');
+let seat2 = document.querySelector('#seat');
+seat2.innerHTML = seatTicket;
+
+let filmsTicket = localStorage.getItem('films');
+let films2 = document.querySelector('#films');
+films2.innerHTML = filmsTicket;
+
